423a875c0a3febffcfdf3b1b1abcedcfbb5a40bb
KaraKartal <k-a-r-a-kartal@hotmail.de>
2018-10-11 22:30:15 +0200

    git update

--- api/userDS.php
+++ api/userDS.php
@@ -52,7 +52,7 @@ function updateCheck() {
     if (domainAvailable('https://github.com/')) {
         $git = shell_exec("git pull");
         $date = date('d-m-Y H:i:s') . "\n";
-        file_put_contents("..\\update.txt", $date . $git . "\n\n", FILE_APPEND);
+        file_put_contents("..\\update.txt", $date . $git . "\n", FILE_APPEND);
     }
 }
 
