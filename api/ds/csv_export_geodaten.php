58878c0b11a2b01b6361dbc8ddf3008ccb600dde
KaraKartal <k-a-r-a-kartal@hotmail.de>
2019-02-10 00:53:59 +0100

    update - 10.02.2019 -  0:53:58

--- api/ds/csv_export_geodaten.php
+++ api/ds/csv_export_geodaten.php
@@ -72,7 +72,7 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] == login && $_SESSION["admin
 
     while (!$rs->EOF) {
 
-        $export .= $rs->fields{'lat'} . ";" . $rs->fields{'lon'} . ";" . utf8_decode($rs->fields{'name'}) . "\r\n";
+        $export .= $rs->fields{'lat'} . ";" . $rs->fields{'lon'} . ";" . ($rs->fields{'name'}) . "\r\n";
 
         $rs->MoveNext();
     }
